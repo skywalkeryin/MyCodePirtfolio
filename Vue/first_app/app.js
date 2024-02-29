110f294c479a609f3322c93912349d44cc1e8870
qianhao <qianhao_yin@calendarone.com>
2024-02-29 19:47:21 +0800

    feat: add the watcher

--- Vue/first_app/app.js
+++ Vue/first_app/app.js
@@ -1,7 +1,32 @@
 Vue.createApp({
-    data() {
-        return {
-            message: 'Hello Vue!'
-          }
+  data() {
+    return {
+      firstName: "John",
+      age: 20,
+      timer: null // Variable to store the timer ID
+    };
+  },
+  methods: {
+    increment() {
+      this.age++;
+    },
+
+    decrement() {
+      this.age--;
+    },
+  },
+
+  watch: {
+    age(newValue, oldValue) {
+        // Clear previous timer if it exists
+        if(this.timer) {
+            clearTimeout(this.timer)
+        }
+
+        this.timer = setTimeout(() => {
+            this.age = 20;
+            console.log("update age");
+          }, 3000);
     }
-}).mount('#app')
+  },
+}).mount("#app");
